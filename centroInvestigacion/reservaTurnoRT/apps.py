518d7c39ddb88fc0ba43f417130387d9b98664ce
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-06-20 01:22:38 -0300

    .

--- centroInvestigacion/reservaTurnoRT/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReservaturnortConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reservaTurnoRT'
--- centroInvestigacion/reservaTurnoRT/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.forms import CharField
-
-class Usuario(models.Model):
-    nombreUsuario = CharField(max_length=10)
-
-
